7906a46db29baeee7542c78c8c5a050bdd8951b4
Alexander Tsybenko <alexander@tsybenko.info>
2013-01-29 15:37:12 +0200

    fix: tabs css

--- sites/all/themes/virdini/page.tpl.php
+++ sites/all/themes/virdini/page.tpl.php
@@ -1,3 +1,7 @@
 <?php include('header.inc'); ?>
+	<?php if ($title): ?><h1><?php echo $title; ?></h1><?php endif; ?>
+  <?php if ($tabs): ?><?php echo $tabs; ?><?php endif; ?>
+  <?php if ($tabs2): ?><?php echo $tabs2; ?><?php endif; ?>
+  <?php if ($show_messages && $messages): ?><div id='console' class='clear-block'><?php echo $messages; ?></div><?php endif; ?>
 	<?php echo $content; ?>
 <?php include('footer.inc'); ?>
